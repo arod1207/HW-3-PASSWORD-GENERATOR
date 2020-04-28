a7dfafd0d2d988962fe8cf15cb5904081293d786
Arod1207 <arod1207@me.com>
2020-04-28 14:52:57 -0500

    Test Commit

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"
 var numbers = ["1","2","3","4","5","6","7","8","9","0"];
 var specialChar = ["!","@","#","$","%","^","&","*","(",")","-","+","_","="];
 var myArray = [];
-var passwordTest = '';
+var randPassword = '';
 
 
 // gathering information from user
@@ -29,19 +29,6 @@ var selectedUpperCase = confirm("Do you want it to containt UPPERCASE letters?")
 var selectedLowerCase = confirm("Do you want it to contain lowercase letters?");
 var selectedNumbers = confirm("Do you want it to contain Numbers?");
 var selectedSpecialChar = confirm("Do you want it to contain special characters?");
-
-
-
-// Password must be between 8 and 128 characters
-
-      if (selectedLength < 8) {
-        alert("Password must be at least 8 characters");
-        
-      }
-     else if (selectedLength > 128) {
-        alert("Password must be less than 128 characters");
-        return;
-      }
 
       
 
@@ -66,14 +53,14 @@ var selectedSpecialChar = confirm("Do you want it to contain special characters?
       var arrayToUse = myArray[Math.floor(Math.random()*myArray.length)];
       var randomCharIndex = Math.floor(Math.random()*arrayToUse.length);
      
-      passwordTest += arrayToUse[randomCharIndex];
+      randPassword += arrayToUse[randomCharIndex];
     }
+
+alert(randPassword);
+
+
       
 
-   
-
-    
-console.log(passwordTest);
 
 
 
@@ -90,35 +77,6 @@ console.log(passwordTest);
 
 
 
-// function writePassword(){
-//     var password = " ";
 
-//       for (i = 0; i < selectedLength; i++){
-//         if (selectedUpperCase === true){
-//           password += upperCase[Math.floor(Math.random() * upperCase.length)];
-//         }
-// //         if (selectedLowerCase === true){
-//           password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-//         }
-//         if (selectedNumbers === true){
-//           password += numbers[Math.floor(Math.random() * numbers.length)];
-//         }
-//         if (selectedSpecialChar === true){
-//           password += specialChar[Math.floor(Math.random() * specialChar.length)];
-//         }
-//     }
-//   alert(password);
-// }
-
-// random code generated
-
-
-// writePassword();
-
-// random generator
-// var randomUpperCase = upperCase[Math.floor(Math.random() * upperCase.length)];
-// var randomLoserCase = lowerCase[Math.floor(Math.random() * lowerCase.length)];
-// var randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
-// var randomCharacteres = specialChar[Math.floor(Math.random() * specialChar.length)];
 
 

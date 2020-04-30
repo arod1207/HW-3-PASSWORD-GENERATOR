de1eb1473276ff95f2adc0fb4c60531c590c58bf
Arod1207 <arod1207@me.com>
2020-04-29 23:38:58 -0500

    Added comments to script.js

--- script.js
+++ script.js
@@ -26,33 +26,29 @@ var myArray = [];
 var randPassword = '';
 
 
-
+//Running this function on button click
 function generatePassword() {
 
   
   var selectedLength = parseInt(prompt("Enter length of password to generate!"));
-
+// checking if an acutal number is inputed into the prompt box
   if (selectedLength === null || isNaN(selectedLength)) {
     alert("This isn't a number, please enter a number");
     return false;
   }
 
-  
+// checking to see length validation 
   if (selectedLength < minCharLength || selectedLength > maxCharLength){
     alert("Need's to be at least 8 Characters or less than or equal to 128 Characters");
     return false;
     
   }
-
-
+// asking user to select password type
   var selectedUpperCase = confirm("Do you want it to containt UPPERCASE letters?");
   var selectedLowerCase = confirm("Do you want it to contain lowercase letters?");
   var selectedNumbers = confirm("Do you want it to contain Numbers?");
   var selectedSpecialChar = confirm("Do you want it to contain special characters?");
-      
- 
-    
-
+// Pushing user selected options to an array if true    
   if (selectedUpperCase === true) {
     myArray.push(upperCase);
   }
@@ -65,18 +61,17 @@ function generatePassword() {
   if (selectedSpecialChar === true) {
     myArray.push(specialChar);
   }
-
-
-
+// running a loop up to the selected password length
   for (var i = 0; i < selectedLength; i ++) {
-
+// randomizing the selected arrays
     var arrayToUse = myArray[Math.floor(Math.random()*myArray.length)];
+// turning the randomized array into a string
     var randomCharIndex = Math.floor(Math.random()*arrayToUse.length);
-    
+// selecting a random character and concatenating them into variable randPassword   
     randPassword += arrayToUse[randomCharIndex];
 
   }
-
+// adding characters into variable
   return randPassword;    
  }
     
